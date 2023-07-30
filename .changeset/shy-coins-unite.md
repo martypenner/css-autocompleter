9a556d4b25fd09114fb814d253cd6b3b00ccc404
Marty Penner <martypenner@users.noreply.github.com>
2023-07-30 15:51:18 -0400

    refactor(parser): center everything around files instead of classes
    This allows invalidation per file. I also added many tests. Woot!

---
+++ .changeset/shy-coins-unite.md
@@ -0,0 +1,7 @@
+---
+'vscode-css-autocomplete': patch
+---
+
+refactor(parser): center everything around files instead of classes.
+
+This allows invalidation per file. I also added many tests. Woot!
--- autocompletion-engine/crates/core/src/parser.rs
+++ autocompletion-engine/crates/core/src/parser.rs
@@ -1,7 +1,6 @@
 use std::{
   collections::{HashMap, HashSet},
   fs::read_to_string,
-  println,
 };
 
 use itertools::Itertools;
@@ -9,7 +8,8 @@ use log::error;
 use tree_sitter::{Parser, Query, QueryCursor};
 
 pub type Completions = Vec<(ClassName, RuleSet)>;
-type IntermediateCompletions = HashMap<ClassName, RuleSetMap>;
+type FileClassMap = HashMap<Filename, HashMap<ClassName, RuleSetMap>>;
+type Filename = String;
 type ClassName = String;
 type RuleSet = String;
 
@@ -19,6 +19,7 @@ type HelpDoc = String;
 
 pub struct AutocompletionEngine {
   completions: Completions,
+  file_class_map: FileClassMap,
   parser: Parser,
   query: Query,
   query_cursor: QueryCursor,
@@ -43,6 +44,7 @@ impl AutocompletionEngine {
 
     Self {
       completions: vec![],
+      file_class_map: HashMap::new(),
       parser,
       query,
       query_cursor: QueryCursor::new(),
@@ -64,19 +66,27 @@ impl AutocompletionEngine {
     self.completions = vec![];
   }
 
-  // TODO: we're currently structured around classnames. This is nice for looping and
-  // creating the final completions list, but makes it harder to invalidate the cache
-  // for a single file. Need to rethink this.
+  pub fn invalidate_file_cache(&mut self, file_path: String) {
+    self.file_class_map.remove(&file_path);
+  }
+
   pub fn get_all_completions_for_files(&mut self, files: Vec<String>) -> &Completions {
-    if !self.completions.is_empty() {
+    // Ensure we don't operate on the same file twice.
+    let unique_files: HashSet<String> = files.into_iter().collect();
+    let files_already_processed = unique_files
+      .iter()
+      .all(|key| self.file_class_map.contains_key(key));
+
+    if files_already_processed {
       return &self.completions;
     }
 
-    // Ensure we don't operate on the same file twice.
-    let files: HashSet<String> = files.into_iter().collect();
-    let mut rule_maps_by_class_name: IntermediateCompletions = HashMap::new();
+    for path in unique_files {
+      if self.file_class_map.contains_key(&path) {
+        continue;
+      }
 
-    for path in files {
+      let mut class_rule_map: HashMap<ClassName, RuleSetMap> = HashMap::new();
       let code = match read_to_string(&path) {
         Ok(code) => code,
         Err(e) => {
@@ -99,7 +109,7 @@ impl AutocompletionEngine {
 
       for each_match in matches {
         let [class_selector, class_name] = each_match.captures else {
-          println!("Could not destructure captures");
+          error!("Could not destructure captures");
           continue;
         };
 
@@ -119,7 +129,7 @@ impl AutocompletionEngine {
         let rule_set_node = match parent {
           Some(p) => p,
           None => {
-            println!(
+            error!(
               "Could not find parent rule set for: {}. Likely a malformed stylesheet.",
               class_selector
                 .utf8_text(code)
@@ -132,7 +142,7 @@ impl AutocompletionEngine {
         let rule_set = match rule_set_node.utf8_text(code) {
           Ok(rule_set) => rule_set,
           Err(e) => {
-            println!(
+            error!(
               "Could not convert node to utf8 text: {}. Error was: {}",
               rule_set_node.kind(),
               e
@@ -145,7 +155,7 @@ impl AutocompletionEngine {
         let class_name = match class_name.utf8_text(code) {
           Ok(class_name) => class_name,
           Err(e) => {
-            println!(
+            error!(
               "Could not convert node to utf8 text: {}. Error was: {}",
               class_name.kind(),
               e
@@ -155,7 +165,7 @@ impl AutocompletionEngine {
         }
         .to_string();
 
-        rule_maps_by_class_name
+        class_rule_map
           .entry(class_name)
           .and_modify(|rule_map| {
             rule_map
@@ -163,25 +173,26 @@ impl AutocompletionEngine {
               .or_insert(rule_set.to_owned());
           })
           .or_insert(HashMap::from([(rule_set_node.id(), rule_set)]));
+
+        self
+          .file_class_map
+          .insert(path.clone(), class_rule_map.clone());
       }
     }
 
-    // Convert intermediate completions into final list.
-    let mut completions: Vec<(String, String)> = vec![];
-
-    // Reserve space for completions to prevent reallocations.
-    completions.reserve(rule_maps_by_class_name.len());
-
+    // Convert the file-centric map into the completions list.
     // I tried a .reserve and .drain approach, and there was no benchmarking
     // improvement. So I opted for the more readable version.
-    for class_name in rule_maps_by_class_name.keys().sorted() {
-      let rule_map = rule_maps_by_class_name.get(class_name).unwrap().to_owned();
-      let rule_sets: Vec<String> = rule_map.into_values().sorted().collect();
-
-      completions.push((class_name.clone(), rule_sets.join("\n\n")));
+    for class_rule_map in self.file_class_map.values() {
+      for (class_name, rule_set_map) in class_rule_map {
+        let rule_sets: Vec<String> = rule_set_map.clone().into_values().sorted().collect();
+        self
+          .completions
+          .push((class_name.clone(), rule_sets.join("\n\n")));
+      }
     }
 
-    self.completions = completions;
+    self.completions.sort_unstable();
     &self.completions
   }
 }
@@ -226,15 +237,15 @@ mod tests {
     [
       (
         "drag-and-drop",
-        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type=\"password\"].focus,\ninput[type=\"password\"]:focus,\ninput[type=\"email\"].focus,\ninput[type=\"email\"]:focus,\ninput[type=\"number\"].focus,\ninput[type=\"number\"]:focus,\ninput[type=\"tel\"].focus,\ninput[type=\"tel\"]:focus,\ninput[type=\"url\"].focus,\ninput[type=\"url\"]:focus,\ninput[type=\"search\"].focus,\ninput[type=\"search\"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
+        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
       ),
       (
         "focus",
-        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type=\"password\"].focus,\ninput[type=\"password\"]:focus,\ninput[type=\"email\"].focus,\ninput[type=\"email\"]:focus,\ninput[type=\"number\"].focus,\ninput[type=\"number\"]:focus,\ninput[type=\"tel\"].focus,\ninput[type=\"tel\"]:focus,\ninput[type=\"url\"].focus,\ninput[type=\"url\"]:focus,\ninput[type=\"search\"].focus,\ninput[type=\"search\"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
+        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
       ),
       (
         "focused",
-        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type=\"password\"].focus,\ninput[type=\"password\"]:focus,\ninput[type=\"email\"].focus,\ninput[type=\"email\"]:focus,\ninput[type=\"number\"].focus,\ninput[type=\"number\"]:focus,\ninput[type=\"tel\"].focus,\ninput[type=\"tel\"]:focus,\ninput[type=\"url\"].focus,\ninput[type=\"url\"]:focus,\ninput[type=\"search\"].focus,\ninput[type=\"search\"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
+        "input[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}"
       ),
       (
         "input-contrast",
@@ -242,7 +253,7 @@ mod tests {
       ),
       (
         "search-page-label",
-        "input[type=\"text\"],\n#adv_code_search .search-page-label,\ninput[type=\"password\"],\ninput[type=\"email\"],\ninput[type=\"number\"],\ninput[type=\"tel\"],\ninput[type=\"url\"],\ninput[type=\"search\"],\ntextarea {\n  min-height: 34px;\n  padding: 7px 8px;\n  font-size: 13px;\n  color: #333;\n  vertical-align: middle;\n  background-color: #fff;\n  background-repeat: no-repeat;\n  background-position: right center;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n  outline: none;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075);\n}\n\ninput[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type=\"password\"].focus,\ninput[type=\"password\"]:focus,\ninput[type=\"email\"].focus,\ninput[type=\"email\"]:focus,\ninput[type=\"number\"].focus,\ninput[type=\"number\"]:focus,\ninput[type=\"tel\"].focus,\ninput[type=\"tel\"]:focus,\ninput[type=\"url\"].focus,\ninput[type=\"url\"]:focus,\ninput[type=\"search\"].focus,\ninput[type=\"search\"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}",
+        "input[type=\"text\"],\n#adv_code_search .search-page-label,\ntextarea {\n  min-height: 34px;\n  padding: 7px 8px;\n  font-size: 13px;\n  color: #333;\n  vertical-align: middle;\n  background-color: #fff;\n  background-repeat: no-repeat;\n  background-position: right center;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n  outline: none;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075);\n}\n\ninput[type=\"text\"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type=\"text\"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}",
       ),
       (
         "wrapper",
@@ -252,38 +263,33 @@ mod tests {
   }
 
   #[test]
-  fn test_parser_failure() {
+  fn file_with_no_classnames_provides_no_completions() {
     let mut engine = AutocompletionEngine::new();
     let completions =
-      engine.get_all_completions_for_files(vec!["invalid_file_path.css".to_string()]);
+      engine.get_all_completions_for_files(vec!["./__test__/no_classnames.css".to_string()]);
     assert!(completions.is_empty());
   }
 
   #[test]
   fn can_get_completions() {
     let mut engine = AutocompletionEngine::new();
-
-    let actual =
-      engine.get_all_completions_for_files(vec!["./__test__/test.atom.io.css".to_string()]);
+    let actual = engine.get_all_completions_for_files(vec!["./__test__/basic.css".to_string()]);
     let expected = get_list();
 
-    for (i, (class_name, rule_set)) in expected.iter().enumerate() {
-      dbg!(class_name);
-      assert_eq!(rule_set.to_string(), actual[i].1);
-    }
-
     assert_eq!(actual.len(), expected.len());
+
+    for (class_name, rule_set) in &expected {
+      assert!(actual.contains(&(class_name.to_string(), rule_set.to_string())));
+    }
   }
 
-  // This is cheating a bit, but I don't have time or energy to inline a long string right now.
   #[test]
   fn can_get_completions_as_string() {
     let mut engine = AutocompletionEngine::new();
-
     let list = get_list();
-    let actual =
-      engine.get_all_completions_as_string(vec!["./__test__/test.atom.io.css".to_string()]);
-    let expected = serde_json::to_string(&list).expect("Could not convert class hashmap to string");
+    let actual = engine.get_all_completions_as_string(vec!["./__test__/basic.css".to_string()]);
+    let expected =
+      serde_json::to_string(&list).expect("Could not convert file-centric map to string");
     assert_eq!(actual, expected);
   }
 
@@ -296,39 +302,98 @@ mod tests {
   }
 
   #[test]
-  fn handles_empty_file_gracefully() {
-    let mut engine = AutocompletionEngine::new();
-    let completions =
-      engine.get_all_completions_for_files(vec!["./__test__/empty.css".to_string()]);
-    assert!(completions.is_empty());
-  }
-
-  #[test]
   fn can_invalidate_cache() {
     let mut engine = AutocompletionEngine::new();
-    engine.get_all_completions_for_files(vec!["./__test__/test.atom.io.css".to_string()]);
-    assert!(!engine.completions.is_empty());
-
-    engine.invalidate_cache();
-    assert!(engine.completions.is_empty());
+    engine.get_all_completions_for_files(vec!["./__test__/basic.css".to_string()]);
+    assert!(!engine.file_class_map.is_empty());
+    engine.invalidate_file_cache("./__test__/basic.css".to_string());
+    assert!(engine
+      .file_class_map
+      .get("./__test__/test.atom.io.css")
+      .is_none());
   }
 
+  // This isn't perfect, but it's something. Not sure how to properly test this.
   #[test]
   fn uses_cache_for_subsequent_calls() {
     let mut engine = AutocompletionEngine::new();
     let first_call = engine
-      .get_all_completions_for_files(vec!["./__test__/test.atom.io.css".to_string()])
+      .get_all_completions_for_files(vec!["./__test__/basic.css".to_string()])
       .clone();
-    let second_call =
-      engine.get_all_completions_for_files(vec!["./__test__/test.atom.io.css".to_string()]);
+    let second_call = engine
+      .get_all_completions_for_files(vec!["./__test__/basic.css".to_string()])
+      .clone();
+    assert_eq!(first_call, second_call);
+  }
 
-    assert_eq!(first_call.len(), second_call.len());
+  #[test]
+  fn test_file_cache_invalidation() {
+    let mut engine = AutocompletionEngine::new();
+    engine.get_all_completions_for_files(vec!["./__test__/basic.css".to_string()]);
+    assert!(!engine.file_class_map.is_empty());
 
-    // The lengths are asserted to be equal, so we can just iterate over
-    // second_call for the comparison.
-    for (i, (class_name, rule_set)) in first_call.iter().enumerate() {
-      assert_eq!(class_name, &second_call[i].0);
-      assert_eq!(rule_set, &second_call[i].1);
-    }
+    engine.invalidate_file_cache("./__test__/basic.css".to_string());
+    assert!(engine.file_class_map.get("./__test__/basic.css").is_none());
+  }
+
+  #[test]
+  fn multiple_files_can_provide_completions() {
+    let mut engine = AutocompletionEngine::new();
+    engine.get_all_completions_for_files(vec![
+      "./__test__/basic.css".to_string(),
+      "./__test__/basic2.css".to_string(),
+    ]);
+
+    let combined_length = engine.file_class_map["./__test__/basic.css"].len()
+      + engine.file_class_map["./__test__/basic2.css"].len();
+    assert_eq!(engine.completions.len(), combined_length);
+  }
+
+  #[test]
+  fn multiple_rulesets_for_single_classname_across_files() {
+    let mut engine = AutocompletionEngine::new();
+    let completions = engine.get_all_completions_for_files(vec![
+      "./__test__/basic.css".to_string(),
+      "./__test__/another_file_with_same_classname.css".to_string(),
+    ]);
+
+    // Locate the class name in question within the completions list
+    let class_entries: Vec<&(String, String)> = completions
+      .iter()
+      .filter(|(class_name, _)| class_name == "wrapper")
+      .collect();
+
+    assert!(class_entries.len() == 2);
+  }
+
+  #[test]
+  fn duplicate_classnames_across_files() {
+    let mut engine = AutocompletionEngine::new();
+    engine.get_all_completions_for_files(vec![
+      "./__test__/basic.css".to_string(),
+      "./__test__/another_file_with_same_classname.css".to_string(),
+    ]);
+
+    let duplicate_class_count = engine
+      .completions
+      .iter()
+      .filter(|(class_name, _)| class_name == "wrapper")
+      .count();
+    assert_eq!(duplicate_class_count, 2);
+  }
+
+  #[test]
+  fn test_file_with_malformed_css() {
+    let mut engine = AutocompletionEngine::new();
+    let completions =
+      engine.get_all_completions_for_files(vec!["./__test__/malformed.css".to_string()]);
+
+    assert_eq!(
+      completions,
+      &[(
+        "wrapper".to_string(),
+        "#peek2 .wrapper {\n  border: 1px solid red;\n}".to_string()
+      )]
+    );
   }
 }
