9120f3636f6aad90b222c3a8d36efd0415fa4a37
Marty Penner <martypenner@users.noreply.github.com>
2023-07-30 17:19:27 -0400

    tests: update js tests to be simpler

--- autocompletion-engine/crates/node/__test__/index.spec.mjs
+++ autocompletion-engine/crates/node/__test__/index.spec.mjs
@@ -17,15 +17,15 @@ test('get completions from test css file', (t) => {
   const expected = [
     [
       'drag-and-drop',
-      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type="password"].focus,\ninput[type="password"]:focus,\ninput[type="email"].focus,\ninput[type="email"]:focus,\ninput[type="number"].focus,\ninput[type="number"]:focus,\ninput[type="tel"].focus,\ninput[type="tel"]:focus,\ninput[type="url"].focus,\ninput[type="url"]:focus,\ninput[type="search"].focus,\ninput[type="search"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
+      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
     ],
     [
       'focus',
-      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type="password"].focus,\ninput[type="password"]:focus,\ninput[type="email"].focus,\ninput[type="email"]:focus,\ninput[type="number"].focus,\ninput[type="number"]:focus,\ninput[type="tel"].focus,\ninput[type="tel"]:focus,\ninput[type="url"].focus,\ninput[type="url"]:focus,\ninput[type="search"].focus,\ninput[type="search"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
+      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
     ],
     [
       'focused',
-      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type="password"].focus,\ninput[type="password"]:focus,\ninput[type="email"].focus,\ninput[type="email"]:focus,\ninput[type="number"].focus,\ninput[type="number"]:focus,\ninput[type="tel"].focus,\ninput[type="tel"]:focus,\ninput[type="url"].focus,\ninput[type="url"]:focus,\ninput[type="search"].focus,\ninput[type="search"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
+      `input[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
     ],
     [
       'input-contrast',
@@ -33,7 +33,7 @@ test('get completions from test css file', (t) => {
     ],
     [
       'search-page-label',
-      `input[type="text"],\n#adv_code_search .search-page-label,\ninput[type="password"],\ninput[type="email"],\ninput[type="number"],\ninput[type="tel"],\ninput[type="url"],\ninput[type="search"],\ntextarea {\n  min-height: 34px;\n  padding: 7px 8px;\n  font-size: 13px;\n  color: #333;\n  vertical-align: middle;\n  background-color: #fff;\n  background-repeat: no-repeat;\n  background-position: right center;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n  outline: none;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075);\n}\n\ninput[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ninput[type="password"].focus,\ninput[type="password"]:focus,\ninput[type="email"].focus,\ninput[type="email"]:focus,\ninput[type="number"].focus,\ninput[type="number"]:focus,\ninput[type="tel"].focus,\ninput[type="tel"]:focus,\ninput[type="url"].focus,\ninput[type="url"]:focus,\ninput[type="search"].focus,\ninput[type="search"]:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
+      `input[type="text"],\n#adv_code_search .search-page-label,\ntextarea {\n  min-height: 34px;\n  padding: 7px 8px;\n  font-size: 13px;\n  color: #333;\n  vertical-align: middle;\n  background-color: #fff;\n  background-repeat: no-repeat;\n  background-position: right center;\n  border: 1px solid #ccc;\n  border-radius: 3px;\n  outline: none;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075);\n}\n\ninput[type="text"].focus,\n#adv_code_search .focus.search-page-label,\ninput[type="text"]:focus,\n.focused .drag-and-drop,\n#adv_code_search .search-page-label:focus,\ntextarea.focus,\ntextarea:focus {\n  border-color: #51a7e8;\n  box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.075),\n    0 0 5px rgba(81, 167, 232, 0.5);\n}`,
     ],
     [
       'wrapper',
